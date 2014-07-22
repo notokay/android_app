b703aef3db328bd8b3007341f860d99d4de50245
Tommy Hu <hutommy@hku.hk>
2014-07-21 22:27:14 -0400

    Created new branch to make the different services into fragments

---
+++ app/src/main/java/com/example/myfirstapp/app/HomeScreen.java
@@ -0,0 +1,57 @@
+package com.example.myfirstapp.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+import com.example.myfirstapp.app.R;
+
+public class HomeScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void launch_bt_chat(View view){
+        Intent btchatIntent = new Intent(this, MainActivity.class);
+        startActivity(btchatIntent);
+    }
+
+    public void launch_arrow_control(View view){
+        Intent arrowIntent = new Intent(this, ArrowControl.class);
+        startActivity(arrowIntent);
+    }
+}
